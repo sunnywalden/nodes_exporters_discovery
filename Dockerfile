01fae51297a1f0d829f9369f634c673abd0d81dd
zhangbo02 <zhangbo02@chinatopcredit.com>
2019-02-19 18:13:27 +0800

    更新服务启动方式及dockerfile

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use centos image as base image
+FROM centos/centos/python-27-centos7:latest
+
+MAINTAINER SHUANGSELOTTERY Docker Maintainers "sunnywalden@gmail.com"
+
+ENV DISCOVERY_PATH /opt/nodes_exporters_discovery
+
+# Make port 8080 available to the world outside this container
+EXPOSE 8888
+
+USER root
+
+# Set the working directory to /opt
+WORKDIR $DISCOVERY_PATH
+
+# Copy lottery release to work dir
+COPY . $DISCOVERY_PATH
+
+# Update pip
+RUN pip install --upgrade pip && pip install --trusted-host pypi.python.org -r requirements.txt
+
+# Change directory to bin path
+WORKDIR $DISCOVERY_PATH/bin
+
+# Start lottery service
+ENTRYPOINT ["uwsgi", "uwsgi.ini"]
---
+++ bin/discovery.py
@@ -0,0 +1,41 @@
+#!venv/bin/flask/bin/python
+# -*- coding:utf-8 -*-
+
+from flask import Flask
+from flask import jsonify
+from logbook import Logger, TimedRotatingFileHandler
+import os
+import sys
+
+BASE_DIR = os.path.abspath(os.path.join(os.getcwd(), ".."))
+sys.path.append(BASE_DIR)
+
+from main.nodes_discovery import NodesDiscovery
+
+app = Flask(__name__)
+
+handler = TimedRotatingFileHandler('../logs/lottery_server.log')
+handler.push_application()
+my_logger = Logger(name='Lottery Server', level=11)
+
+
+@app.route('/nodes/api/v1', methods=['GET'])
+def get_nodes():
+    """
+    Parse interface to get numbers then request the lottery funtion to get it and post the res to user.
+
+    """
+    my_logger.info("Start nodes scan")
+    nodes_discover = NodesDiscovery()
+    nodes_discover.nodes_file_backup()
+    try:
+        nodes_discover.node_scan()
+    except Exception as e:
+        my_logger.error(e)
+        return jsonify({'status': 'failed', 'msg': '接口返回错误 %s' % e})
+    else:
+        my_logger.info('Node discovery finished!')
+        return jsonify({'status': 'success', 'msg': '扫描完成'})
+    nodes_discover.send_metrics()
+
+
